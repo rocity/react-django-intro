9e96dd1cb0fcdbaf71112507fd1c2755cfda2d9e
Kevin Kalis <kevin@swiftkind.com>
2017-05-05 00:02:03 +0800

    Add static to urls

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
